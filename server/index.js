83ace9661e1bff75d80dd7d60cf425fa6d62b35e
jacobxq <zhihua0113@hotmail.com>
2018-03-08 18:30:55 +0800

    使用koa-views搭建模版

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 const Koa = require('koa')
-// const ejs = require('ejs')
-const pug = require('pug')
-
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
-
+const views = require('koa-views')
 const app = new Koa()
+const {resolve} = require('path')
+
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html;charset=utf-8'
-  ctx.body = pug.render(pugTpl, {
-    name: 'Jocob',
-    des: '搞事情'
+  await ctx.render('index', {
+    name: "Jacob",
+    des: '搞事情views，还有谁'
   })
 })
+
+// app.use(async (ctx, next) => {
+//   ctx.type = 'text/html;charset=utf-8'
+//   ctx.body = pug.render(pugTpl, {
+//     name: 'Jocob',
+//     des: '搞事情'
+//   })
+// })
 
 app.listen(5566)
