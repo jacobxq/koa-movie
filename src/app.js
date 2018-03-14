81a01883f8e7a66201dec67378df5f6e30a67a52
jacobxq <zhihua0113@hotmail.com>
2018-03-14 09:48:02 +0800

    修复无法显示问题

--- src/app.js
+++ src/app.js
@@ -7,12 +7,22 @@ import Routes from './routes'
 import 'antd/dist/antd.css'
 import './assets/common.sass'
 
-export default () => {
-  <Switch>
-    {
-      Routes.map(({ name, path, exact = true, component }) => (
-        <Route path={path} exact={exact} component={component} key={name} />
-      ))
-    }
-  </Switch>
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <Switch>
+        {
+          Routes.map(({ name, path, exact = true, component }) => (
+            <Route path={path} exact={exact} component={component} key={name} />
+          ))
+        }
+      </Switch>
+    )
+  }
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {
-  BrowserRouter
-} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 
-render(<BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('app'))
+render(
+  (
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>
+  ),
+  document.getElementById('app'))
